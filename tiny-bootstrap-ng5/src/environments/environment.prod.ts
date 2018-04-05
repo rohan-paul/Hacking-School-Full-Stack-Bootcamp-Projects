c14428fb34f77193bddba4e3dc1ee80431e4a6a7
Rohan Paul <rohanpaul2@gmail.com>
2018-04-06 00:20:38 +0530

    correcting folder structure

---
+++ tiny-bootstrap-ng5/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBiVr9LZwh74Ojhz8GbhfSMzTxUvNXobXY",
+    authDomain: "tiny-bootstrap-ng5.firebaseapp.com",
+    databaseURL: "https://tiny-bootstrap-ng5.firebaseio.com",
+    projectId: "tiny-bootstrap-ng5",
+    storageBucket: "",
+    messagingSenderId: "126646444103"
+  }
+};
