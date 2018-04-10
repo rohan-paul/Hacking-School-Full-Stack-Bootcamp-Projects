8ce7eb77601a73419a93f648b4e5e04d3427288f
Rohan Paul <rohanpaul2@gmail.com>
2018-04-10 23:18:53 +0530

    matrix addition alternative ways and code explanation

--- JS-Challenges/matrix-addition.js
+++ JS-Challenges/matrix-addition.js
@@ -11,10 +11,12 @@ For Matrix Visualization see how the 2 2-D Arrays in LHS added up to give the re
 |1 1 1|   |1 1 3|   |2 2 4|
 
 Also note that the lengths of the 2 2-d arrays are same. i.e arr1.length == arr2.length in the below problem.
+
+Basically, to do matrix addition simply add up the values inside the inner-array-1 with the corresponding values found on the inner-Array-2.
   */
 // Alterntive-1
 function matrixAddition (arr1, arr2) {
-  let result = [];
+  let result = []; // the final output which is also a 2-d array
   for (var i = 0; i < arr1.length; i++) {
     result[i] = []; // set this first element of the resulting array to be an empty array, and I am going to fill this index position up with the next lines of codes
     for (var j = 0; j < arr1.length; j++) {
@@ -26,7 +28,7 @@ function matrixAddition (arr1, arr2) {
 
 // Alternative-2, same approach but using forEach() instead of for()
 function matrixAddition (arr1, arr2) {
-  let result = [];
+  let result = []; // the final output which is also a 2-d array
   arr1.forEach(function (arrItem, index1) {
     let sums = [];
     arrItem.forEach(function (numItem, index2) {
@@ -36,8 +38,21 @@ function matrixAddition (arr1, arr2) {
   });
   return result;
 }
-console.log(matrixAddition([ [1, 2, 3], [3, 2, 1,], [1, 1, 1] ], [ [2, 2, 1], [3, 2, 3], [1, 1, 3] ] ));
+// console.log(matrixAddition([ [1, 2, 3], [3, 2, 1,], [1, 1, 1] ], [ [2, 2, 1], [3, 2, 3], [1, 1, 3] ] ));
 
 /* Further notes on the above
-A> When I am looping through the first depth of the outer 2dArray arr1, which values are also arrays. To do matrix addition simply add up the values inside the inner arrays with the corresponding values found on the inner 2dArray b.
+A> First, I am looping through the first depth of the outer 2dArray arr1, which values are also arrays.
+B> The first argument numItem in arr1.forEach is the value of the current index in the outer 2d Array arr1. The argument index1 is an index of 2dArray arr1 where the value numItem is found.
+C> We can use index1 to reference the corresponding inner array on 2dArray arr2 from the inner iteration arrItem.forEach and use the arrItem.forEach index argument which I labeled index2 to reference the numbers in 2dArray b by reading arr2[index1][index2].
 */
+
+// The most concise mechanism using .map functiion
+function matrixAddition (arr1, arr2) {
+  return arr1.map(function (n, i) {
+    return n.map(function(o, j) {
+      return o + arr2[i][j];
+    });
+  });
+}
+
+console.log(matrixAddition([ [1, 2, 3], [3, 2, 1,], [1, 1, 1] ], [ [2, 2, 1], [3, 2, 3], [1, 1, 3] ] ));
