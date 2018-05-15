7959d2dea74b5ded87577bf78f006bfeb075d16e
Rohan Paul <rohanpaul2@gmail.com>
2018-05-15 23:42:16 +0530

    react assignment - return users and followers from json data - passing data from parent to child

--- random-code-2.js
+++ random-code-2.js
@@ -1,30 +1,19 @@
-var fs = require('fs');
-var path = require('path');
+let users = [
+        {
+            id:1,
+            name: 'A',
+            followers: [2,3]
+        },
+        {
+            id:2,
+            name: 'B',
+            followers: [3]
+        },
+        {
+            id:3,
+            name: 'C',
+            followers: [1]
+        }
+    ];
 
-// Windows?
-var win32 = process.platform === 'win32';
-// Normalize \\ paths to / paths.
-function unixifyPath(filepath) {
-  if (win32) {
-    return filepath.replace(/\\/g, '/');
-  } else {
-    return filepath;
-  }
-};
-
-// Recurse into a directory, executing callback for each file.
-function walk(rootdir, callback, subdir) {
-  var abspath = subdir ? path.join(rootdir, subdir) : rootdir;
-  fs.readdirSync(abspath).forEach(function(filename) {
-    var filepath = path.join(abspath, filename);
-    if (fs.statSync(filepath).isDirectory()) {
-      walk(rootdir, callback, unixifyPath(path.join(subdir || '', filename || '')));
-    } else {
-      callback(unixifyPath(filepath), rootdir, subdir, filename);
-    }
-  });
-};
-
-walk(__dirname, function(filepath, rootdir, subdir, filename) {
-  console.log('Filepath', filepath);
-});
+    console.log(users);
