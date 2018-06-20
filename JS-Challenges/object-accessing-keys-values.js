7167aa06d0490c283db086f50d4784256c798f1b
Rohan Paul <rohanpaul2@gmail.com>
2018-06-20 14:13:20 +0530

    accessing key from an object given the value

--- JS-Challenges/object-accessing-keys-values.js
+++ JS-Challenges/object-accessing-keys-values.js
@@ -31,9 +31,31 @@ console.log(Object.values(obj));  // => [ 1, 2, 3 ]
   If the key is not yet known when we write the code, then we must use object[key] but be careful, sometimes we need to wrap the key in quotes:
   object["key"] like when the key is not a variable. */
 
-  // Write a function to return the key from an object given a value
-  findKeyFromValue = (obj, value) => {
-  	return Object.keys(obj).find(key => obj[key] === value);
-  }
+ // Write a function to return the key from an object given a value
+ // Both Object.keys and Object.values will return me an array, so I can further apply find() or indexOf() methods on them
+findKeyFromValue1 = (obj, value) => {
+	return Object.keys(obj).find(key => obj[key] === value);
+}
 
-  console.log(findKeyFromValue(obj, 2));  // => [ 1, 2, 3 ]
+// console.log(findKeyFromValue1(obj, 2));  // => [ 1, 2, 3 ]
+
+// Next alternative to find the key given the value
+findKeyFromValue2 = (obj, value) => {
+	return Object.keys(obj).filter(key => obj[key] === value)[0];
+}
+
+// console.log(findKeyFromValue2(obj, 2));  // => [ 1, 2, 3 ]
+
+/* A) Next alternative to find the key given the value
+ 
+ B) Here, to get the key I am using the syntax array[inedexNo]. Like array[0] will return me the first element of the array
+ 
+ C) So, I am first getting the array containing all the keys with Object.keys. Then I am getting an array containing all the values with Object.values. Then I am fincing the indes no of the values that I want with indexOf()
+
+ D) Finally I am doing keys[indexNo] kind of syntax
+ */
+findKeyFromValue3 = (obj, value) => {
+	return Object.keys(obj)[Object.values(obj).indexOf(value)]
+}
+
+console.log(findKeyFromValue3(obj, 2));  // => [ 1, 2, 3 ]
