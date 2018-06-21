0fa4e3f3ccab95f6bb16985bb0f94cd9271ca944
Rohan Paul <rohanpaul2@gmail.com>
2018-06-21 23:57:15 +0530

    shuffle array so all zeros are at the beginning

--- JS-Challenges/9-complement.js
+++ JS-Challenges/9-complement.js
@@ -33,3 +33,11 @@ nineComplementSum = num => {
 }
 
 console.log(nineComplementSum(104));
+
+/* Further read this - https://www.electrical4u.com/9s-complement-and-10s-complement/
+
+Now coming to 10's complement, it is relatively easy to find out the 10's complement after finding out the 9,s complement of that number. We have to add 1 with the 9's complement of any number to obtain the desired 10's complements.
+
+If 9's complement is 543 then 10's complement of this no 544
+
+*/
