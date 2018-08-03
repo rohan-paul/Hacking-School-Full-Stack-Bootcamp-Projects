49685a7c523e2cf657c9598115bd2208151bd7ae
Rohan Paul <rohanpaul2@gmail.com>
2018-08-04 00:12:12 +0530

    alt solution to generate primes

--- JS-Challenges/prime-generation-between-Range.js
+++ JS-Challenges/prime-generation-between-Range.js
@@ -1,4 +1,4 @@
-// Print all primes between range - faced this in Techolution Interview on 21-June-2018
+// SOLUTION-1 - Print all primes between range - faced this in Techolution Interview on 21-June-2018
 
 isPrime = (num) => {
 
@@ -36,3 +36,21 @@ generatePrime = (a, b) => {
 }
 
 console.log(generatePrime(1, 10));
+
+// SOLUTION-2
+
+allPrimes = n => {
+  let range = [...Array(n + 1).keys()].slice(2);
+  return range.filter(p => isPrime2(p))
+}
+
+isPrime2 = n => {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if ( n % i === 0) return false;
+  } return true;
+}
+
+console.log(allPrimes(10));
+
+
+
